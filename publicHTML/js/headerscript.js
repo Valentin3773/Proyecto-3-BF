86e9f2e9fed4f85093037cb6c5fa8f33bcc78c05
Valentin3773 <chichovalentim@gmail.com>
2024-03-30 04:02:08 -0300

    Animaciones

--- publicHTML/js/headerscript.js
+++ publicHTML/js/headerscript.js
@@ -1,17 +1,36 @@
 window.addEventListener('scroll', checkButtonPosition);
+window.addEventListener('scroll', checkHeaderPosition);
 
 let footer = document.querySelector('footer');
 let button = document.querySelector('#btnchat');
 
 function checkButtonPosition() {
+
     let bounding = footer.getBoundingClientRect();
 
     if (bounding.top <= window.innerHeight) {
+        
         document.querySelector('#btnchat').style.animation = 'chatup 0.6s ease forwards';
-        console.log("Poroto")
-    } else {
+    } 
+    else {
+        
         document.querySelector('#btnchat').style.animation = 'chatdown 0.6s ease forwards';
-
     }
 }
+
+function checkHeaderPosition() {
+
+    let header = document.querySelector('header');
+
+    if(window.scrollY > 0) {
+
+        header.className = "comprimido";
+    }
+    else {
+
+        header.className = "extendido";
+    }
+}
+
+
 
