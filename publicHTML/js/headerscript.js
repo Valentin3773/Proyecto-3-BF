ad2815e65922c14e6e69601a48b54c9c40823da0
Valentin3773 <chichovalentim@gmail.com>
2024-04-07 19:28:33 -0300

    Cambios en el CSS y adición de Bootstrap

--- publicHTML/js/headerscript.js
+++ publicHTML/js/headerscript.js
@@ -5,25 +5,29 @@ let footer = $('footer');
 let button = $('#btnchat');
 
 function checkButtonPosition() {
+    
     var posFooter = $('footer').get(0).getBoundingClientRect();
 
     if (posFooter.top <= $(window).height()) {
+        
         $('#btnchat').css({ 'animation': 'chatup 0.6s ease forwards' });
+    } 
+    else {
 
-    } else {
         $('#btnchat').css({ 'animation': 'chatdown 0.6s ease forwards' });
-
     }
 
 }
-
-
 function checkHeaderPosition() {
+
     var header = $('header');
 
     if ($(window).scrollTop() > 1) {
+
         header.addClass('comprimido').removeClass('extendido');
-    } else {
+    } 
+    else {
+
         header.addClass('extendido').removeClass('comprimido');
     }
 }
