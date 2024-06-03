7772396a6bf0149a782be7a0ff58e449cabf51be
Valentin3773 <chichovalentim@gmail.com>
2024-06-03 17:09:21 -0300

    aaaaa

--- publicHTML/backend/loginmanager.php
+++ publicHTML/backend/loginmanager.php
@@ -1,48 +1,53 @@
 <?php
-    include("conexion.php");
 
-    // Verifica si el formulario ha sido enviado
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['ingresar'])) {
-        
-        // Verifica si el campo 'jejeje' no está vacío
-        if (empty(trim($_POST["jejeje"]))) {
-            processCheckUser($pdo);
-        } else {
-            echo "Fuera Hacker!!!";
-        }
+include("conexion.php");
+
+// Verifica si el formulario ha sido enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['ingresar'])) {
+    
+    // Verifica si el campo 'jejeje' no está vacío
+    if (empty(trim($_POST["jejeje"]))) {
+
+        processCheckUser($pdo);
+    } 
+    else {
+
+        echo "Fuera bot hijueputa!!!";
     }
+}
 
-    function processCheckUser($pdo) {
-        $email = trim($_POST["email"]);
-        $contrasenia = trim($_POST["contrasenia"]);
+function processCheckUser($pdo) {
 
-        // Prepara la consulta SQL para verificar si el usuario existe
-            $chekeo = "SELECT email, contrasenia FROM paciente WHERE email = :email AND contrasenia = :contrasenia";
-            $stmt = $pdo->prepare($chekeo);
+    $email = trim($_POST["email"]);
+    $contrasenia = trim($_POST["contrasenia"]);
 
-            //Prepara la variable stmt para ejecutar, con el agregado que el PDO trate el dato como String
-            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-            $stmt->bindParam(':contrasenia', $contrasenia, PDO::PARAM_STR);
+    // Prepara la consulta SQL para verificar si el usuario existe
+    $chekeo = "SELECT email, contrasenia FROM paciente WHERE email = :email AND contrasenia = :contrasenia";
+    $stmt = $pdo->prepare($chekeo);
 
-        // Ejecuta la consulta
-        if ($stmt->execute()) {
-            if ($stmt->rowCount() > 0) {
-                echo "Usuario existente";
+    // Prepara la variable stmt para ejecutar, con el agregado que el PDO trate el dato como String
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->bindParam(':contrasenia', $contrasenia, PDO::PARAM_STR);
 
-                //Esto redirige al URL especificado
-                header("location:../index.html");
-                exit();
-            } else {
-                echo "Usuario no existente";
-            }
-        } else {
+    // Ejecuta la consulta
+    if ($stmt->execute()) {
+
+        if ($stmt->rowCount() > 0) {
+
+            echo "Usuario existente";
+            header("location:../index.html");
+            exit();
+        } 
+        else {
+
+            echo "Usuario no existente";
+        }
+    } 
+    else {
+
         echo "Error al ejecutar la consulta";
-        }
-        // Cierra la sentencia y la conexión
-        unset($stmt);
-        unset($pdo);
     }
-
-
-
-?>
+    // Cierra la sentencia y la conexión
+    unset($stmt);
+    unset($pdo);
+}
--- publicHTML/backend/registromanager.php
+++ publicHTML/backend/registromanager.php
@@ -1,68 +1,78 @@
 <?php
-    include("conexion.php");
 
-    // Verifica si el formulario ha sido enviado
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['registrarse'])) {
-        
-        // Verifica si el campo 'jejeje' está vacío
-        if (empty(trim($_POST["jejeje"]))) {
-            processRegisterForm($pdo);
-        } else {
-            echo "Fuera Hacker!!!";
-        }
+include("conexion.php");
+
+// Verifica si el formulario ha sido enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['registrarse'])) {
+
+    // Verifica si el campo 'jejeje' está vacío
+    if (empty(trim($_POST["jejeje"]))) {
+
+        processRegisterForm($pdo);
+    } 
+    else {
+
+        echo "Fuera bot hijueputa!!!";
     }
+}
 
-    function processRegisterForm($pdo)
-    {
-        // Obtiene los datos del formulario
-        $nombre = trim($_POST["nombre"]);
-        $apellidos = trim($_POST["apellido"]);
-        $documento = trim($_POST["documento"]);
-        $telefono = trim($_POST["telefono"]);
-        $direccion = trim($_POST["direccion"]);
-        $email = trim($_POST["email"]);
-        $contrasenia = trim($_POST["contrasenia"]);
-        $concontrasenia = trim($_POST["concontrasenia"]);
+function processRegisterForm($pdo) {
 
-        // Comprueba si las contraseñas coinciden
-        if ($contrasenia == $concontrasenia) {
+    // Obtiene los datos del formulario
+    $nombre = trim($_POST["nombre"]);
+    $apellidos = trim($_POST["apellido"]);
+    $documento = trim($_POST["documento"]);
+    $telefono = trim($_POST["telefono"]);
+    $direccion = trim($_POST["direccion"]);
+    $email = trim($_POST["email"]);
+    $contrasenia = trim($_POST["contrasenia"]);
+    $concontrasenia = trim($_POST["concontrasenia"]);
 
-            // Prepara la consulta SQL para verificar si el documento ya existe
-            $chekeo = "SELECT documento FROM paciente WHERE documento = :documento";
-            $stmt = $pdo->prepare($chekeo);
-            $stmt->bindParam(':documento', $documento);
+    // Comprueba si las contraseñas coinciden
+    if ($contrasenia == $concontrasenia) {
 
-            // Ejecuta la consulta
-            if ($stmt->execute()) {
-                
-                // Verifica si se encontró algún registro
-                if ($stmt->rowCount() > 0) {
-                    echo "Usuario ya registrado";
-                } else {
+        // Prepara la consulta SQL para verificar si el documento ya existe
+        $chekeo = "SELECT documento FROM paciente WHERE documento = :documento";
+        $stmt = $pdo->prepare($chekeo);
+        $stmt->bindParam(':documento', $documento);
 
-                    // Prepara la consulta SQL para insertar el nuevo registro
-                    $sql = "INSERT INTO paciente (documento, nombre, apellido, email, contrasenia, direccion, telefono) VALUES (?, ?, ?, ?, ?, ?, ?)";
-                    $stmt = $pdo->prepare($sql);
+        // Ejecuta la consulta
+        if ($stmt->execute()) {
 
-                    // Ejecuta la consulta de inserción
-                    if ($stmt->execute([$documento, $nombre, $apellidos, $email, $contrasenia, $direccion, $telefono])) {
-                        
-                        //Esto redirige al URL especificado
-                        header("Location: https://youtu.be/mCdA4bJAGGk?si=T0061-A3bbfF476Z");
-                        exit();
-                    } else {
-                        echo "Lo siento! Se ha presentado un error.";
-                    }
+            // Verifica si se encontró algún registro
+            if ($stmt->rowCount() > 0) {
+
+                echo "Usuario ya registrado";
+            } 
+            else {
+
+                // Prepara la consulta SQL para insertar el nuevo registro
+                $sql = "INSERT INTO paciente (documento, nombre, apellido, email, contrasenia, direccion, telefono) VALUES (?, ?, ?, ?, ?, ?, ?)";
+                $stmt = $pdo->prepare($sql);
+
+                // Ejecuta la consulta de inserción
+                if ($stmt->execute([$documento, $nombre, $apellidos, $email, $contrasenia, $direccion, $telefono])) {
+
+                    //Esto redirige al URL especificado
+                    header("Location: https://youtu.be/mCdA4bJAGGk?si=T0061-A3bbfF476Z");
+                    exit();
+                } 
+                else {
+
+                    echo "Lo siento! Se ha presentado un error.";
                 }
-            } else {
-                echo "Error al ejecutar la consulta.";
             }
-        } else {
-            echo "Ingrese correctamente las contraseñas";
-        }
+        } 
+        else {
 
-        // Cierra la sentencia y la conexión
-        unset($stmt);
-        unset($pdo);
+            echo "Error al ejecutar la consulta.";
+        }
+    } 
+    else {
+
+        echo "Ingrese correctamente las contraseñas";
     }
-?>
+    // Cierra la sentencia y la conexión
+    unset($stmt);
+    unset($pdo);
+}
