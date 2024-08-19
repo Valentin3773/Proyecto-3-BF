f2398747579431adbb6a5e520a581c4396a7a728
Santiago Vargas <santiagomarquez732@gmail.com>
2024-08-19 01:43:25 -0300

    Integracion about us
    Se desarrollo about  us.

---
+++ publicHTML/aboutUs/aboutus.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/publicHTML/lib/bootstrap-5.2.3-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/sectionmid.css">
+    <link rel="stylesheet" href="css/footer.css">
+
+</head>
+
+<body>
+    <div>
+        <header>
+            <h1 id="aboutus">About Us</h1>
+        </header>
+    </div>
+
+    <section>
+        <div id="logopa">
+            <img id="logo" src="img/PA.png" alt="Logo de la Program Army">
+        </div>
+        <p id="titulo"> La Program Army</p>
+
+    </section>
+
+    <div id="contenedormision">
+        <pre id="mision">
+
+                                                                &lt;Mission&gt;
+              We focus on creating applications and solutions of high quality and 
+                                        adaptability for any of our clients' needs.
+                                                                &lt;/Mission&gt;                     
+        </pre>
+
+    </div>
+
+    <div id="contenedorvision">
+        <pre id="vision">
+
+                                              &lt;Vision&gt;
+                Being an average company leading the inner 
+                                    market of software in Uruguay.
+                                              &lt;/Vision&gt;                     
+        </pre>
+    </div>
+
+    <p id="miembros">Member of Company</p>
+
+    <div id="contenedormiembros">
+        <h1 id="leader">Leader</h1>
+        <img id="imgl" src="img/image 22.png" alt="Leader">
+        <h1 id="manager">Manager</h1>
+        <img id="imgm" src="img/image 22.png" alt="Manager">
+        <h1 id="developer"> Developer</h1>
+        <img id="imgd" src="img/image 22.png" alt="Developer">
+        <h1 id="developer2">Developer</h1>
+        <img id="imgd2" src="img/image 22.png" alt="Developer">
+        <h1 id="subleader">Sub Leader</h1>
+        <img id="imgsl" src="img/image 22.png" alt="Sub Leader">
+    </div>
+
+    <div>
+        <footer>
+            <h1 id="derechos">©️ Todos los derechos reservados</h1>
+            <div id="contenedorflogo">
+                <img id="flogo" src="img/PA.png" alt="La Program Army">
+            </div>
+        </footer>
+    </div>
+
+
+</body>
+
+</html>
