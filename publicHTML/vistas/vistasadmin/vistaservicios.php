cc42f2b064186571fd1b7369d8a8fffa1f860885
Valentin3773 <chichovalentim@gmail.com>
2024-09-11 18:04:10 -0300

    pancracio

--- publicHTML/vistas/vistasadmin/vistaservicios.php
+++ publicHTML/vistas/vistasadmin/vistaservicios.php
@@ -4,48 +4,89 @@ include("../../backend/conexion.php");
 
 session_start();
 
-if(!isset($_SESSION['odontologo'])) header('Location: ../../index.php');
+if (!isset($_SESSION['odontologo'])) header('Location: ../../index.php');
 
-if(!isset($_GET['numservicio'])) {
+if (!isset($_GET['numservicio'])) {
 
     $sql = "SELECT * FROM servicio ORDER BY nombre ASC";
 
     $stmt = $pdo->prepare($sql);
 
-    if($stmt->execute()) $servicios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($stmt->execute()) $servicios = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    ?>
+?>
     <div id="tituservicioscontainer" class="d-flex justify-content-center gx-0 mt-3">
 
         <h1 id="tituloservicios" class="text-center">Servicios</h1>
 
-    </div> 
-    <div id="servicioscontainer"> 
-    <?php
+    </div>
+    <div id="servicioscontainer">
+        <?php
 
-    foreach($servicios as $servicio)
+        foreach ($servicios as $servicio)
 
-        echo "<div id='{$servicio["numero"]}' class='servicio'><h3>{$servicio["nombre"]}<h3></div>";
+            echo "<div id='{$servicio["numero"]}' class='servicio'><h3>{$servicio["nombre"]}<h3></div>";
 
-    ?> </div> <?php
-} 
-else if(is_numeric($_GET['numservicio']) && $_GET['numservicio'] > 0) {
-    
-    $numservicio = $_GET['numservicio'];
+        ?> </div> <?php
+                } else if (is_numeric($_GET['numservicio']) && $_GET['numservicio'] > 0) {
 
-    $sql = "SELECT * FROM servicio WHERE numero = :numservicio ORDER BY nombre ASC";
+                    $numservicio = $_GET['numservicio'];
 
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':numservicio', $numservicio);
+                    $sql = "SELECT * FROM servicio WHERE numero = :numservicio ORDER BY nombre ASC";
 
-    if($stmt->execute() && $stmt->rowCount() == 1) $servicio = $stmt->fetch(PDO::FETCH_ASSOC);
-?>
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->bindParam(':numservicio', $numservicio);
+
+                    if ($stmt->execute() && $stmt->rowCount() == 1) $servicio = $stmt->fetch(PDO::FETCH_ASSOC);
+                    ?>
 
     <div id="cuerpo">
 
         <h1><?= $servicio['nombre'] ?></h1>
-        <br><br><br><b><br><br><br><br><br><br><br><br><br>
-        <div class="form-group">
+
+        <div id="contimg" class="my-5">
+
+            <div id="iconocontainer">
+
+                <span class="leyenda">Icono</span>
+
+                <div id="icowrapper">
+
+                    <?php
+                    if (isset($servicio["icono"])) echo "<img src='backend/almacenamiento/icoservicios/{$servicio["icono"]}' alt='Icono del servicio' id='icoservicio'>";
+
+                    else echo "<img src='img/logaso.png' alt='Icono del servicio' id='icoservicio'>";
+                    ?>
+
+                    <div class="lapizeditar">
+                        <img src="img/iconosvg/lapiz.svg" id="mdF" alt="Modificar" title="Modificar">
+                    </div>
+
+                </div>
+
+            </div>
+            <div id="imgcontainer">
+
+                <span class="leyenda">Imagen</span>
+
+                <div id="imgwrapper">
+
+                    <?php
+                    if (isset($servicio["imagen"])) echo "<img src='backend/almacenamiento/imgservicios/{$servicio["imagen"]}' alt='Imagen del servicio' id='imgservicio'>";
+
+                    else echo "<img src='img/logaso.png' alt='Imagen del servicio' id='imgservicio'>";
+                    ?>
+
+                    <div class="lapizeditar">
+                        <img src="img/iconosvg/lapiz.svg" id="mdF" alt="Modificar" title="Modificar">
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="form-group p-3">
             <label for="descripcion">Descripcion</label>
             <textarea name="descripcion" id="descripcion"></textarea>
         </div>
@@ -53,5 +94,5 @@ else if(is_numeric($_GET['numservicio']) && $_GET['numservicio'] > 0) {
     </div>
 
 <?php
-}
+                }
 ?>
